b3249babc2156402ea9579476338602f1d6a8f58
sarath246 <59415242+sarath246@users.noreply.github.com>
2020-10-20 02:03:42 +0530

    New class added

---
+++ String-pgm/src/com/java/string/program/StringFIrstLast.java
@@ -0,0 +1,23 @@
+package com.java.string.program;
+
+public class StringFIrstLast {
+
+	public static void main(String[] args) {
+
+		
+		String str = "apple";
+		char rev = 0;
+		
+		String str2 = new String("apple");
+		
+		System.out.println("First char :" + str2.charAt(0));
+		System.out.println("Last char :" + str2.charAt(str2.length()-1));
+		System.out.println("len :" + str2.length());
+		
+		for (int i = str2.length()-1; i >=0; i--) {
+	
+			
+		}
+	}
+
+}
